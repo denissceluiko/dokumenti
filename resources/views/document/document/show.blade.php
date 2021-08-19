7eb6e016722be306c10297968339bae7eaa65275
Deniss Celuiko <deniss.celuiko@gmail.com>
2021-08-19 23:41:29 +0300

    Added binding display for a document

--- resources/views/document/document/show.blade.php
+++ resources/views/document/document/show.blade.php
@@ -3,6 +3,21 @@
 @section('title', $document->name)
 
 @section('content')
-<h1>{{ $document->name }}</h1>
-<a href="{{ route('document.download', $document) }}">Download</a>
+    <h1>{{ $document->name }}</h1>
+    <a href="{{ route('document.download', $document) }}">Download</a>
+    <a href="{{ route('document.download', $document) }}">Regenerate</a>
+    <table>
+    @foreach($document->bindings['bindings'] as $binding => $value)
+        <tr>
+            <td>{{ $binding }}</td>
+            <td>{{ $value }}</td>
+        </tr>
+    @endforeach
+    @foreach($document->bindings['rows'] as $row => $value)
+        <tr>
+            <td>{{ $row }}</td>
+            <td><pre>{{ json_encode($value, JSON_PRETTY_PRINT) }}</pre></td>
+        </tr>
+    @endforeach
+    </table>
 @endsection
