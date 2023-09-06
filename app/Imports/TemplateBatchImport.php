4975dad045129691296ca66d739a6c161c99440e
Deniss Celuiko <deniss.celuiko@gmail.com>
2023-09-06 14:24:31 +0300

    Will calculate formulas

--- app/Imports/TemplateBatchImport.php
+++ app/Imports/TemplateBatchImport.php
@@ -6,9 +6,10 @@ use App\Jobs\CompileTemplateBatch;
 use App\Models\Document\Template;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
+use Maatwebsite\Excel\Concerns\WithCalculatedFormulas;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
-class TemplateBatchImport implements ToCollection, WithHeadingRow
+class TemplateBatchImport implements ToCollection, WithHeadingRow, WithCalculatedFormulas
 {
     protected $template;
 
